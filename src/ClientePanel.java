9373b68548879ad12d1782401b1054909f07f5ac
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-05 19:54:28 -0300

    Feat: Criar métodos para adicionar nos campos de cadastro e criar botão para eles e criar método para limpar campos

--- src/ClientePanel.java
+++ src/ClientePanel.java
@@ -8,4 +8,49 @@ public class ClientePanel extends JPanel {
     private JTextField telefoneField;
     private JTextField placaCarroField;
     private JTextField modeloCarroField;
+
+    public ClientePanel() {
+        setLayout(new GridLayout(5, 2));
+
+        JLabel nomeLabel = new JLabel("Nome:");
+        nomeField = new JTextField();
+        JLabel telefoneLabel = new JLabel("Telefone:");
+        telefoneField = new JTextField();
+        JLabel placaCarroLabel = new JLabel("Placa carro:");
+        placaCarroField = new JTextField();
+        JLabel modeloCarroLabel = new JLabel("Modelo carro:");
+        modeloCarroField = new JTextField();
+
+        JButton cadastrarButton = new JButton("Cadastrar Cliente");
+        cadastrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nome = nomeField.getText();
+                String telefone = telefoneField.getText();
+                String placaCarro = placaCarroField.getText();
+                String modeloCarro = modeloCarroField.getText();
+
+                JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso!");
+                clearFields();
+
+
+            }
+        });
+
+        add(nomeLabel);
+        add(nomeField);
+        add(telefoneLabel);
+        add(telefoneField);
+        add(placaCarroLabel);
+        add(placaCarroField);
+        add(modeloCarroLabel);
+        add(modeloCarroField);
+        add(cadastrarButton);
+
+    }
+
+    public void clearFields() {
+
+
+    }
 }
