b864fee888057bb344c6bd0ec501200b2753cd19
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-05 20:36:50 -0300

    Feat: Criar classe para cuidar dos agendamentos dos clientes com os métodos

---
+++ src/AgendamentoPanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+public class AgendamentoPanel extends JPanel {
+    private JTextField clienteField;
+    private JTextField servicoField;
+    private JTextField dataHoraField;]
+    private J
+}
