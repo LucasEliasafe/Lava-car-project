c278bba4498431728e45aa25e39d11991096ced7
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-05 19:48:02 -0300

    Feat: Criar classe para gerenciar o cadastro dos clientes

---
+++ src/ClientePanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ClientePanel extends JPanel {
+    private JTextField nomeField;
+    private JTextField telefoneField;
+    private JTextField placaCarroField;
+    private JTextField modeloCarroField;
+}
